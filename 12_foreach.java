f22fa24ec2b73356956d9da2e1bceba91c0b408e
Tanmay09516 <85993243+Tanmay09516@users.noreply.github.com>
2022-04-17 23:07:27 +0530

    Update and rename xiii_foreach.java to 12_foreach.java

--- 12_foreach.java
+++ 12_foreach.java
@@ -1,5 +1,5 @@
 // wap to demonstrate for each loop
-public class xiii_foreach {
+public class foreach {
     public static void main(String args[]){
         int[] marks = {1,2,3,4,5,6};
         for (int mark:marks)
