e6f60ac022fb158d62ec28cef40e50209cd31d83
Tanmay09516 <85993243+Tanmay09516@users.noreply.github.com>
2022-04-27 22:13:10 +0530

    Update stringmethods.py

--- stringmethods.py
+++ stringmethods.py
@@ -2,14 +2,12 @@ import java.lang.String;
  class stringmethods {
     public static void main(String args[] ) 
     {
-        String test= "  abcvbslbfasdn";
+        String test= "  abcvbslbf";
         System.out.println(test.length());
-        System.out.println(test.IndexOf("a"));
         System.out.println(test.isBlank());
         System.out.println(test.isEmpty());
         System.out.println(test.substring(4));
-        System.out.println(test.substring(10,11));
-        System.out.println(test.substring(-2,-1));
+        System.out.println(test.substring(3,5));
         char[] chararray=test.toCharArray();
         for(int i=0;i<chararray.length;i++)
         {
@@ -17,3 +15,20 @@ import java.lang.String;
         }
     }
 }
+ 
+#  11
+# false
+# false
+# cvbslbf
+# bc
+# index: 0:  
+# index: 1:
+# index: 2: a
+# index: 3: b
+# index: 4: c
+# index: 5: v
+# index: 6: b
+# index: 7: s
+# index: 8: l
+# index: 9: b
+# index: 10: f
