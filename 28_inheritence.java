8617eb0e6b37418a3bbe2de55b81a4efc333c22d
Tanmay09516 <85993243+Tanmay09516@users.noreply.github.com>
2022-05-04 21:56:58 +0530

    Create 28_inheritence.java

---
+++ 28_inheritence.java
@@ -0,0 +1,18 @@
+interface print
+{
+    void print1();
+}
+class test implements print
+{
+    public void print1()
+    {
+        System.out.println("interface print1");
+    }
+}
+public class interface1 {
+    public static void main(String args[])
+    {
+        test obj=new test();
+        obj.print1();
+    } 
+}
